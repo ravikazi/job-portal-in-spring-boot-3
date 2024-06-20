a19beb6c873f5e28bbacaa105e5ec24bc0f9614b
ravikumarkazi <ravikumarkazi@gmail.com>
2024-06-20 15:58:26 +0530

    Recruiter Dashboard

--- src/main/java/com/webmayura/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/webmayura/jobportal/controller/JobPostActivityController.java
@@ -1,59 +1,82 @@
 package com.webmayura.jobportal.controller;
 
 import com.webmayura.jobportal.entity.JobPostActivity;
+import com.webmayura.jobportal.entity.RecruiterJobsDto;
+import com.webmayura.jobportal.entity.RecruiterProfile;
 import com.webmayura.jobportal.entity.Users;
 import com.webmayura.jobportal.services.JobPostActivityService;
 import com.webmayura.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
+
     private final UsersService usersService;
     private final JobPostActivityService jobPostActivityService;
 
     @Autowired
-    public JobPostActivityController(UsersService usersService,JobPostActivityService jobPostActivityService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService) {
         this.usersService = usersService;
-        this.jobPostActivityService=jobPostActivityService;
+        this.jobPostActivityService = jobPostActivityService;
     }
 
     @GetMapping("/dashboard/")
-    public String searchJob(Model model){
+    public String searchJobs(Model model) {
+
         Object currentUserProfile = usersService.getCurrentUserProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (!(authentication instanceof AnonymousAuthenticationToken)){
-            String currentUsername= authentication.getName();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
-        model.addAttribute("user",currentUserProfile);
+
+        model.addAttribute("user", currentUserProfile);
+
         return "dashboard";
     }
 
     @GetMapping("/dashboard/add")
-    public String addJob(Model model){
+    public String addJobs(Model model) {
         model.addAttribute("jobPostActivity", new JobPostActivity());
         model.addAttribute("user", usersService.getCurrentUserProfile());
         return "add-jobs";
     }
 
     @PostMapping("/dashboard/addNew")
-    public String addNew(JobPostActivity jobPostActivity, Model model){
+    public String addNew(JobPostActivity jobPostActivity, Model model) {
+
         Users user = usersService.getCurrentUser();
-        if (user!=null){
+        if (user != null) {
             jobPostActivity.setPostedById(user);
         }
         jobPostActivity.setPostedDate(new Date());
-        model.addAttribute("jobPostActivity",jobPostActivity);
+        model.addAttribute("jobPostActivity", jobPostActivity);
         JobPostActivity saved = jobPostActivityService.addNew(jobPostActivity);
         return "redirect:/dashboard/";
+    }
+
+    @PostMapping("/dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model){
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity",jobPostActivity);
+        model.addAttribute("user",usersService.getCurrentUserProfile());
+        return "add-jobs";
     }
 }
